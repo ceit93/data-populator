573514e1a37e2353eedebb4b33acf7bc332f03f2
Amir Haghighati <haghighati.amir@gmail.com>
2018-11-16 18:12:59 -0500

    add new file stringify

---
+++ src/stringify.js
@@ -0,0 +1,22 @@
+module.exports = function stringify(data) {
+    let cache = [];
+    let json = JSON.stringify(data, function(key, value) {
+        if (typeof value === 'object' && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+                // Duplicate reference found
+                try {
+                    // If this value does not reference a parent it can be deduped
+                    return JSON.parse(JSON.stringify(value));
+                } catch (error) {
+                    // discard key if value cannot be deduped
+                    return;
+                }
+            }
+            // Store value in our collection
+            cache.push(value);
+        }
+        return value;
+    });
+    cache = null
+    return json
+}
